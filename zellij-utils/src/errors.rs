45ccbb8b8cbaa4781695319145b383bc208cd632
Aram Drevekenin <aram@poor.dev>
2026-03-14 08:35:00 +0100

    Ansi dump support (#4848)
    * add missing pane-id and tab-id arguments to all cli actions
    
    * rustfmt
    
    * dump-screen --ansi
    
    * edit-scrollback --ansi
    
    * add --ansi to subscribe
    
    * add tests for ansi additions
    
    * add ansi render report to plugins
    
    * rustfmt
    
    * fix e2e tests
    
    * add pr

--- zellij-utils/src/errors.rs
+++ zellij-utils/src/errors.rs
@@ -444,6 +444,7 @@ pub enum ScreenContext {
     PreviousSwapLayoutWithTabId,
     NextSwapLayoutWithTabId,
     MoveTabWithTabId,
+    PluginSubscribedToAnsiPaneContents,
 }
 
 /// Stack call representations corresponding to the different types of [`PtyInstruction`]s.
